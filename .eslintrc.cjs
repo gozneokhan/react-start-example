87e6fc0371f8d54713de666f67cdf5a9f608db1e
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-05-02 22:33:50 +0900

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,6 @@ module.exports = {
         'react/jsx-no-target-blank': 'off',
         'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
         'no-unused-vars': 'off',
-        'react/prop-type': 'off',
+        'react/prop-types': 'off',
     },
 };
